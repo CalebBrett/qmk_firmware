e9a3a91a1870f29556d8a4c80ebaea9e44831646
calebbrett <calebbrett44@gmail.com>
2023-02-21 02:49:14 -0500

    Add steno and nk rollover

--- keyboards/keebio/iris/keymaps/Calebs/keymap.c
+++ keyboards/keebio/iris/keymaps/Calebs/keymap.c
@@ -1,23 +1,23 @@
 #include QMK_KEYBOARD_H
 
 
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 3
+#define _MAIN 0
+#define _ALTERNATE 1
+#define _APPS 2
+#define _STENO 3
 
 // changed #define TAPPING_TOGGLE 2 from 5 in action_tapping.h to toggle with TT() in only 2 presses
 
 enum custom_keycodes {
-    QWERTY = SAFE_RANGE,
-    LOWER,
-    RAISE,
-    ADJUST,
+    MAIN = SAFE_RANGE,
+    ALTERNATE,
+    APPS,
+    STENO,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = LAYOUT(
+  [_MAIN] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -25,13 +25,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      TT(1),           OSL(2),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
+     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   TT(_ALTERNATE),   OSL(_APPS),KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LGUI, KC_LALT,   KC_SPC,                   KC_ENT,  KC_LBRC, KC_RBRC
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_LOWER] = LAYOUT(
+  [_ALTERNATE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
       KC_F12,  KC_F1,   KC_F2,   KC_F3,  KC_F4,   KC_F5,                                KC_F6,   KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -39,61 +39,77 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
       KC_P4,   KC_P5,   KC_P6,  KC_LEFT, KC_DOWN, KC_RIGHT,                           _______, _______, _______, KC_BRID, RGB_HUD, RGB_VAD,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-      KC_P1,   KC_P2,   KC_P3,   KC_P0,  KC_PAST, KC_PSLS,  TO(0),           _______, RGB_M_P, RGB_M_B, RGB_M_SW, _______, _______, RGB_TOG,
+      KC_P1,   KC_P2,   KC_P3,   KC_P0,  KC_PAST, KC_PSLS,  TO(_MAIN),           _______, _______, _______, _______, _______, _______, RGB_TOG,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_MUTE, KC_MPLY, KC_LCTL,                   MACRO00, KC_BSLS,  KC_GRV
+                                    KC_MUTE, KC_MPLY, KC_LCTL,                   MACRO00,   KC_BSLS,  KC_GRV
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_RAISE] = LAYOUT(
+  [_APPS] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+      MACRO01,  _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+     _______, _______, _______, _______, _______, _______,                            KC_PPLS, KC_PMNS, KC_PAST, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______, LCA(KC_DEL),        TO(0), _______, _______, _______, _______, _______, _______,
+     _______, _______, _______, _______, _______, _______, LCA(KC_DEL),      TO(_MAIN), _______, _______, _______, _______, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_CALC, KC_MAIL, KC_MSEL,                   KC_SLEP, KC_PSCR, MAGIC_HOST_NKRO
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_ADJUST] = LAYOUT(
+  [_STENO] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+     MACRO02  ,_______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+     _______,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+     _______,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,  KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, _______,                   _______, _______, _______
+                                    _______,   KC_C,    KC_V,                      KC_N,    KC_M,  _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case QWERTY:
+        case MAIN:
             if (record->event.pressed) {
-                set_single_persistent_default_layer(_QWERTY);
+                set_single_persistent_default_layer(_MAIN);
             }
             return false;
             break;
-        case LOWER:
+        case ALTERNATE:
             if (record->event.pressed) {
-                layer_on(_LOWER);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+                layer_on(_ALTERNATE);
+                update_tri_layer(_ALTERNATE, _APPS, _STENO);
             } else {
-                layer_off(_LOWER);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+                layer_off(_ALTERNATE);
+                update_tri_layer(_ALTERNATE, _APPS, _STENO);
             }
             return false;
             break;
         case MACRO00:
-            SEND_STRING("\n/all gg\n"); //Good Game! :)?
+            if (record->event.pressed) {
+                SEND_STRING("\n/all Good Game! :)\n");
+            }
+            return false;
+            break;
+        case MACRO01:
+            if (record->event.pressed) {
+                tap_code16(LAG(KC_ESC));
+                layer_move(_STENO);
+            }
+            return false;
+            break;
+        case MACRO02:
+            if (record->event.pressed) {
+                tap_code16(LSG(KC_ESC));
+                layer_move(_MAIN);
+            }
             return false;
             break;
     }
@@ -121,14 +137,20 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
-        case _RAISE:
-            rgblight_sethsv(0x27,  0xFF, 0xFF);
+        case _STENO:
+            rgblight_mode(12);
             break;
-        case _LOWER:
-            rgblight_sethsv(0x60,  0xFF, 0xFF);
+        case _APPS:
+            if (rgblight_get_mode() != 2) { rgblight_mode(2); }
+            rgblight_sethsv(0x1E,  0xFF, 0xFF);
             break;
-        case _QWERTY:
-            rgblight_sethsv(0xB8,  0xFF, 0xFF);
+        case _ALTERNATE:
+            if (rgblight_get_mode() != 2) { rgblight_mode(2); }
+            rgblight_sethsv(0x75,  0xFF, 0xFF);
+            break;
+        case _MAIN:
+            if (rgblight_get_mode() != 2) { rgblight_mode(2); }
+            rgblight_sethsv(0x40,  0xFF, 0xFF);
             break;
     }
   return state;

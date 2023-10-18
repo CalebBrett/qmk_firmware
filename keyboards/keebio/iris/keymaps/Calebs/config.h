e5c9621163facc8b119acd3c18a16f25d8226c1b
CalebBrett <calebbrett44@gmail.com>
2023-10-17 23:43:37 -0300

    Fix nkro. Change layout.

--- keyboards/keebio/iris/keymaps/Calebs/config.h
+++ keyboards/keebio/iris/keymaps/Calebs/config.h
@@ -25,3 +25,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TOGGLE 2
 // Turn off lights when computer is off
 #define RGBLIGHT_SLEEP
+#define RGBLIGHT_SPLIT
